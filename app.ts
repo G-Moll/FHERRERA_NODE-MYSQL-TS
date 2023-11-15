9e23cbf89930db77b45e4da34a1c321e326fa698
G-Moll <36389559+G-Moll@users.noreply.github.com>
2023-11-15 03:00:16 -0600

    Setup Express

--- app.ts
+++ app.ts
@@ -1,3 +1,10 @@
+import dotenv from 'dotenv';
+import Server from './models/server';
+
+dotenv.config();
+
 export const name = "Jesùs";
 console.log( name );
 
+const server = new Server();
+server.listen();
