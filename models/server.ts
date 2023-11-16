b2f046357ed44b81d43a6f2fa8c740fc9189a2c7
G-Moll <36389559+G-Moll@users.noreply.github.com>
2023-11-15 23:48:57 -0600

    Hot fixes

--- models/server.ts
+++ models/server.ts
@@ -10,7 +10,7 @@ class Server {
     }
 
     listen() {
-        this.app.listen( this.port, () => {
+        this.app.listen( this.port, function() {
             console.log( "Server running on port " + this.port );
         });
     }
