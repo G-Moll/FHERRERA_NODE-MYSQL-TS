24a948bb8351f9d61d6dba716b26c95d91b81dd4
G-Moll <36389559+G-Moll@users.noreply.github.com>
2023-11-22 07:01:06 -0600

    GET Users/User

--- controllers/users.controller.ts
+++ controllers/users.controller.ts
@@ -1,18 +1,27 @@
 import { Request, Response } from 'express';
+import User from '../models/user.model';
 
-export const getUsers = ( req: Request, res: Response ) => {
+export const getUsers = async( req: Request, res: Response ) => {
+    const users = await User.findAll();
     res.json({
-        msg: 'getUsers'
+        users: users
     });
 }
 
-export const getUser = ( req: Request, res: Response ) => {
+export const getUser = async( req: Request, res: Response ) => {
     const { id } = req.params;
+    const user = await User.findByPk( id );
 
-    res.json({
-        msg: 'getUser',
-        id: id
-    });
+    if( !user ) {
+        res.status( 404 ).json({
+            msg: `User with id ${ id } doesn't exist...`
+        });
+    }
+    else {
+        res.json({
+            user: user
+        });
+    }
 }
 
 export const postUser = ( req: Request, res: Response ) => {
